1ba54df69bb26c788a9a68fcbeef1f35c6133c82
nareshbabu-007 <nareshbabu231468@gmail.com>
2024-07-06 21:18:16 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,22 @@
+import react from 'react';
+import {Outlet,Link} from "react-router-dom";
+const Form=()=>{
+    return(
+        <>
+        <nav>
+            <ol>
+                <li>
+                    <Link to ="/">Registration</Link>
+                </li>
+                <li>
+                    <Link to ="/login">Log in</Link>
+                </li>
+                <li>
+                    <Link to ="/signup">sign up</Link>
+                </li>
+            </ol>
+        </nav>
+        </>
+    );
+}
+export default Form;
